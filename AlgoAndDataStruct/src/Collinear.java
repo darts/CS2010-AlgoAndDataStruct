ae32c7be583ac42a62ee259d904408721be917b8
dartse <darts@tcd.ie>
2018-09-25 20:35:52 +0100

    Initial commit

---
+++ AlgoAndDataStruct/src/Collinear.java
@@ -0,0 +1,204 @@
+// -------------------------------------------------------------------------
+/**
+ * This class contains only two static methods that search for points on the
+ * same line in three arrays of integers.
+ *
+ * @author
+ * @version 18/09/18 12:21:09
+ */
+class Collinear {
+
+	// ----------------------------------------------------------
+	/**
+	 * Counts for the number of non-hoizontal lines that go through 3 points in
+	 * arrays a1, a2, a3. This method is static, thus it can be called as
+	 * Collinear.countCollinear(a1,a2,a3)
+	 * 
+	 * @param a1: An UNSORTED array of integers. Each integer a1[i] represents the
+	 *        point (a1[i], 1) on the plain.
+	 * @param a2: An UNSORTED array of integers. Each integer a2[i] represents the
+	 *        point (a2[i], 2) on the plain.
+	 * @param a3: An UNSORTED array of integers. Each integer a3[i] represents the
+	 *        point (a3[i], 3) on the plain.
+	 * @return the number of points which are collinear and do not lie on a
+	 *         horizontal line.
+	 *
+	 *         Array a1, a2 and a3 contain points on the horizontal line y=1, y=2
+	 *         and y=3, respectively. A non-horizontal line will have to cross all
+	 *         three of these lines. Thus we are looking for 3 points, each in a1,
+	 *         a2, a3 which lie on the same line.
+	 *
+	 *         Three points (x1, y1), (x2, y2), (x3, y3) are collinear (i.e., they
+	 *         are on the same line) if
+	 * 
+	 *         x1(y2−y3)+x2(y3−y1)+x3(y1−y2)=0
+	 *
+	 *         In our case y1=1, y2=2, y3=3.
+	 *
+	 *         You should implement this using a BRUTE FORCE approach (check all
+	 *         possible combinations of numbers from a1, a2, a3)
+	 *
+	 *         ----------------------------------------------------------
+	 *
+	 * 
+	 *         Order of Growth -------------------------
+	 *
+	 *         Caclulate and write down the order of growth of your algorithm. You
+	 *         can use the asymptotic notation. You should adequately explain your
+	 *         answer. Answers without adequate explanation will not be counted.
+	 *
+	 *         Order of growth: N^3
+	 *
+	 *         Explanation: The three nested for() loops in the code means that the
+	 *         order of growth is N * N * N. This is because each loop is executing
+	 *         N-times every time the previous loop executes once hence N * N * N =
+	 *         N^3.
+	 */
+	static int countCollinear(int[] a1, int[] a2, int[] a3) {
+		int y1 = 1, y2 = 2, y3 = 3, count = 0;
+		if (a1 != null && a2 != null && a3 != null) {
+			for (int i = 0; i < a1.length; i++) {
+				int x1 = a1[i];
+				for (int j = 0; j < a2.length; j++) {
+					int x2 = a2[j];
+					for (int k = 0; k < a3.length; k++) {
+						int x3 = a3[k];
+						if (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2) == 0) {
+							count++;
+						}
+					}
+				}
+			}
+		}
+		return count;
+	}
+
+	// ----------------------------------------------------------
+	/**
+	 * Counts for the number of non-hoizontal lines that go through 3 points in
+	 * arrays a1, a2, a3. This method is static, thus it can be called as
+	 * Collinear.countCollinearFast(a1,a2,a3)
+	 * 
+	 * @param a1: An UNSORTED array of integers. Each integer a1[i] represents the
+	 *        point (a1[i], 1) on the plain.
+	 * @param a2: An UNSORTED array of integers. Each integer a2[i] represents the
+	 *        point (a2[i], 2) on the plain.
+	 * @param a3: An UNSORTED array of integers. Each integer a3[i] represents the
+	 *        point (a3[i], 3) on the plain.
+	 * @return the number of points which are collinear and do not lie on a
+	 *         horizontal line.
+	 *
+	 *         In this implementation you should make non-trivial use of
+	 *         InsertionSort and Binary Search. The performance of this method
+	 *         should be much better than that of the above method.
+	 *
+	 *
+	 *         Order of Growth -------------------------
+	 *
+	 *         Caclulate and write down the order of growth of your algorithm. You
+	 *         can use the asymptotic notation. You should adequately explain your
+	 *         answer. Answers without adequate explanation will not be counted.
+	 *
+	 *         Order of Growth: N^2
+	 *
+	 *         Explanation: Two nested for loops each running N times, results in N
+	 *         * N = N^2 order of growth.
+	 *
+	 *
+	 */
+	static int countCollinearFast(int[] a1, int[] a2, int[] a3) {
+		int y1 = 1, y2 = 2, y3 = 3, count = 0;
+		if (a1 != null && a2 != null && a3 != null) {
+			for (int i = 0; i < a1.length; i++) {
+				int x1 = a1[i];
+				for (int j = 0; j < a2.length; j++) {
+					int x2 = a2[j];
+					int toFind = (x1 * (y2 - y3) + x2 * (y3 - y1));
+					if (Collinear.binarySearch(a3, toFind))
+						count++;
+				}
+			}
+		}
+		return count;
+	}
+
+	// ----------------------------------------------------------
+	/**
+	 * Sorts an array of integers according to InsertionSort. This method is static,
+	 * thus it can be called as Collinear.sort(a)
+	 * 
+	 * @param a: An UNSORTED array of integers.
+	 * @return after the method returns, the array must be in ascending sorted
+	 *         order.
+	 *
+	 *         ----------------------------------------------------------
+	 *
+	 *         Order of Growth -------------------------
+	 *
+	 *         Caclulate and write down the order of growth of your algorithm. You
+	 *         can use the asymptotic notation. You should adequately explain your
+	 *         answer. Answers without adequate explanation will not be counted.
+	 *
+	 *         Order of Growth: N^2
+	 *
+	 *         Explanation: Two nested linear for-loops.
+	 *
+	 */
+	static void sort(int[] a) {
+		for (int j = 1; j < a.length; j++) {
+			int i = j - 1;
+			while (i >= 0 && a[i] > a[i + 1]) {
+				int temp = a[i];
+				a[i] = a[i + 1];
+				a[i + 1] = temp;
+				i--;
+			}
+		}
+	}
+
+	// ----------------------------------------------------------
+	/**
+	 * Searches for an integer inside an array of integers. This method is static,
+	 * thus it can be called as Collinear.binarySearch(a,x)
+	 * 
+	 * @param a: A array of integers SORTED in ascending order.
+	 * @param x: An integer.
+	 * @return true if 'x' is contained in 'a'; false otherwise.
+	 *
+	 *         ----------------------------------------------------------
+	 *
+	 *         Order of Growth -------------------------
+	 *
+	 *         Caclulate and write down the order of growth of your algorithm. You
+	 *         can use the asymptotic notation. You should adequately explain your
+	 *         answer. Answers without adequate explanation will not be counted.
+	 *
+	 *         Order of Growth: log(N)
+	 *
+	 *         Explanation: Each iteration removes half of all possible outcomes are
+	 *         removed from the search set.
+	 *
+	 */
+	static boolean binarySearch(int[] a, int x) {
+		if (a != null) {
+			Collinear.sort(a);
+			int low = 0, high = a.length - 1, mid = low + ((high - low) / 2);
+			if (x >= a[low] && x <= a[high]) {
+				while (high >= low) {
+					mid = low + ((high - low) / 2);
+					int midNum = a[mid];
+					if (midNum == x) {
+						System.out.println(midNum + "    " + x);
+						return true;
+					} else if (midNum < x) {
+						low = mid + 1;
+					} else {
+						high = mid - 1;
+					}
+				}
+			}
+		}
+		return false;
+	}
+
+}
---
+++ AlgoAndDataStruct/src/CollinearTest.java
@@ -0,0 +1,166 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Arrays;
+
+//-------------------------------------------------------------------------
+/**
+ * Test class for Collinear.java
+ *
+ * @author
+ * @version 18/09/18 12:21:26
+ */
+@RunWith(JUnit4.class)
+public class CollinearTest {
+	// ~ Constructor ........................................................
+	@Test
+	public void testConstructor() {
+		new Collinear();
+	}
+
+	// ~ Public Methods ........................................................
+
+	// ----------------------------------------------------------
+	/**
+	 * Check that the two methods work for empty arrays
+	 */
+	@Test
+	public void testEmpty() {
+		int expectedResult = 0;
+
+		assertEquals("countCollinear failed with 3 empty arrays", expectedResult,
+				Collinear.countCollinear(new int[0], new int[0], new int[0]));
+		assertEquals("countCollinearFast failed with 3 empty arrays", expectedResult,
+				Collinear.countCollinearFast(new int[0], new int[0], new int[0]));
+	}
+
+	// ----------------------------------------------------------
+	/**
+	 * Check for no false positives in a single-element array
+	 */
+	@Test
+	public void testSingleFalse() {
+		int[] a3 = { 15 };
+		int[] a2 = { 5 };
+		int[] a1 = { 10 };
+
+		int expectedResult = 0;
+
+		assertEquals("countCollinear({10}, {5}, {15})", expectedResult, Collinear.countCollinear(a1, a2, a3));
+		assertEquals("countCollinearFast({10}, {5}, {15})", expectedResult, Collinear.countCollinearFast(a1, a2, a3));
+	}
+
+	// ----------------------------------------------------------
+	/**
+	 * Check for no false positives in a single-element array
+	 */
+	@Test
+	public void testSingleTrue() {
+		int[] a3 = { 15, 5 };
+		int[] a2 = { 5 };
+		int[] a1 = { 10, 15, 5 };
+
+		int expectedResult = 1;
+
+		assertEquals(
+				"countCollinear(" + Arrays.toString(a1) + "," + Arrays.toString(a2) + "," + Arrays.toString(a3) + ")",
+				expectedResult, Collinear.countCollinear(a1, a2, a3));
+		assertEquals("countCollinearFast(" + Arrays.toString(a1) + "," + Arrays.toString(a2) + "," + Arrays.toString(a3)
+				+ ")", expectedResult, Collinear.countCollinearFast(a1, a2, a3));
+	}
+
+	// ---------------------------------------------------------
+	/*
+	 * a Evaluate all binarySearch cases.
+	 *
+	 */
+	@Test
+	public void evalBinarySearch() {
+		int[] theArray = { 1, 7, 3, 5, 2 }; // 1, 2, 3, 5, 7
+
+		boolean expectedResult = true;
+		int numToFind = 3;
+		assertEquals("binarySearch(" + Arrays.toString(theArray) + "," + numToFind, expectedResult,
+				Collinear.binarySearch(theArray, numToFind));
+
+		// If num to find is less than mid
+		numToFind = 1;
+		assertEquals("binarySearch(" + Arrays.toString(theArray) + "," + numToFind, expectedResult,
+				Collinear.binarySearch(theArray, numToFind));
+
+		// If num to find is greater than mid
+		numToFind = 7;
+		assertEquals("binarySearch(" + Arrays.toString(theArray) + "," + numToFind, expectedResult,
+				Collinear.binarySearch(theArray, numToFind));
+
+		// If the array is large.
+		int[] bigArray = { 1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 };
+		numToFind = 2;
+		assertEquals("binarySearch(" + Arrays.toString(theArray) + "," + numToFind, expectedResult,
+				Collinear.binarySearch(bigArray, numToFind));
+
+		// If the array is large.
+		numToFind = 20;
+		assertEquals("binarySearch(" + Arrays.toString(theArray) + "," + numToFind, expectedResult,
+				Collinear.binarySearch(bigArray, numToFind));
+
+		// If num to find is not in the array
+		expectedResult = false;
+		numToFind = 8;
+		assertEquals("binarySearch(" + Arrays.toString(theArray) + "," + numToFind, expectedResult,
+				Collinear.binarySearch(bigArray, numToFind));
+		
+		//If the num to find is greater than the highest num in the array
+		numToFind = 30;
+		assertEquals("binarySearch(" + Arrays.toString(theArray) + "," + numToFind, expectedResult,
+				Collinear.binarySearch(bigArray, numToFind));
+		
+		//If the num to find is lower than the lowest num in the array
+		numToFind = -1;
+		assertEquals("binarySearch(" + Arrays.toString(theArray) + "," + numToFind, expectedResult,
+				Collinear.binarySearch(bigArray, numToFind));
+		
+		// If the array is null
+		theArray = null;
+		assertEquals("binarySearch(" + Arrays.toString(theArray) + "," + numToFind, expectedResult,
+				Collinear.binarySearch(theArray, numToFind));
+	}
+
+	// Check for null arrays in search functions.
+	@Test
+	public void checkForNull() {
+
+		int[] a1 = { 15, 5 };
+		int[] a2 = { 5 };
+		int[] a3 = { 10, 15, 5 };
+
+		int expectedResult = 0;
+		a1 = null;
+		assertEquals("countCollinear(" + "NULL" + "," + Arrays.toString(a2) + "," + Arrays.toString(a3) + ")",
+				expectedResult, Collinear.countCollinear(a1, a2, a3));
+		assertEquals("countCollinearFast(" + "NULL" + "," + Arrays.toString(a2) + "," + Arrays.toString(a3) + ")",
+				expectedResult, Collinear.countCollinearFast(a1, a2, a3));
+
+		a2 = null;
+		a1 = new int[2];
+		a1[0] = 15;
+		a1[1] = 5;
+		assertEquals("countCollinear(" + Arrays.toString(a1) + "," + "NULL" + "," + Arrays.toString(a3) + ")",
+				expectedResult, Collinear.countCollinear(a1, a2, a3));
+		assertEquals("countCollinearFast(" + Arrays.toString(a1) + "," + "NULL" + "," + Arrays.toString(a3) + ")",
+				expectedResult, Collinear.countCollinearFast(a1, a2, a3));
+
+		a3 = null;
+		a2 = new int[1];
+		a2[0] = 5;
+		assertEquals("countCollinear(" + Arrays.toString(a1) + "," + Arrays.toString(a2) + "," + "NULL" + ")",
+				expectedResult, Collinear.countCollinear(a1, a2, a3));
+		assertEquals("countCollinearFast(" + Arrays.toString(a1) + "," + Arrays.toString(a2) + "," + "NULL" + ")",
+				expectedResult, Collinear.countCollinearFast(a1, a2, a3));
+
+	}
+
+}
